eab3da56677c78ce8164285aa811a3594e1cd24a
Armando <armando@Armando.local>
2015-03-07 23:58:05 +0100

    Separated the coredata stack

---
+++ GrumpyGif/CoreDataStack.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStack.h
+//  GrumpyGif
+//
+//  Created by Armando on 07/03/15.
+//  Copyright (c) 2015 Armando Carmona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+- (instancetype)initWithModelName:(NSString *)modelName;
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+@end
