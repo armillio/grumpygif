49808dc38cf71575d3ab9159bf3bca0b7bcc267b
Armando <armando@Armando.local>
2015-03-07 22:21:02 +0100

    Basic CollectionView done

--- GrumpyGif/MainViewController.m
+++ GrumpyGif/MainViewController.m
@@ -7,7 +7,91 @@
 //
 
 #import "MainViewController.h"
+#import "UIColor+RandomColors.m"
+#import "MainViewCell.h"
+
+NSString *const kCellIdentifier = @"collectionCell";
+
+@interface MainViewController() <UINavigationControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout>
+@property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) UICollectionViewFlowLayout *regularLayout;
+@property (nonatomic, assign) CGFloat statusBarHeight;
+@property (nonatomic, assign) CGFloat navigationBarHeight;
+@property (nonatomic, strong) NSArray *gifArray;
+@end
 
 @implementation MainViewController
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self setStatusAndNavigationHeightVariables];
+}
+
+-(void) setStatusAndNavigationHeightVariables{
+    self.statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+    self.navigationBarHeight = self.navigationController.navigationBar.frame.size.height;
+}
+
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.title = @"GrumpyGif";
+    
+    [self loadImageData];
+    [self loadLayout];
+    [self loadCollectionView];
+    [self loadCollectionCell];
+}
+
+-(void) loadImageData{
+    self.gifArray = @[@"giphy.gif"];
+}
+
+-(void) loadCollectionView{
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds
+                                             collectionViewLayout:self.regularLayout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    self.collectionView.backgroundColor = [UIColor randomColor];
+    [self.view addSubview:self.collectionView];
+}
+
+-(void) loadCollectionCell{
+    [self.collectionView registerClass:[MainViewCell class] forCellWithReuseIdentifier:kCellIdentifier];
+}
+
+-(void) loadLayout
+{
+    self.regularLayout = [[UICollectionViewFlowLayout alloc] init];
+    self.regularLayout.minimumInteritemSpacing = 0;
+    self.regularLayout.minimumLineSpacing = 0;
+    self.regularLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    self.regularLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    self.regularLayout.itemSize = CGSizeMake(self.view.frame.size.width,
+                                             (self.view.frame.size.height - self.statusBarHeight - self.navigationBarHeight) / 6);
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView
+    numberOfItemsInSection:(NSInteger)section{
+
+    return self.gifArray.count;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                 cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    MainViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier forIndexPath:indexPath];
+    
+    if(cell == nil){
+        cell = [[MainViewCell alloc] init];
+    }
+    
+    NSString *imageName = self.gifArray[indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:imageName];
+    //image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imageName]];
+    
+    return cell;
+}
 
 @end
