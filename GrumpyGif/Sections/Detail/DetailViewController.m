53c161f0a01da7e7bb94ed24f905734b64d9c555
Armando <armando@Armando.local>
2015-03-13 15:55:43 +0100

    Added images, and properties to the detail view

--- GrumpyGif/Sections/Detail/DetailViewController.m
+++ GrumpyGif/Sections/Detail/DetailViewController.m
@@ -17,32 +17,43 @@
 @interface DetailViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *detailImageView;
 @property (weak, nonatomic) IBOutlet UIButton *shareImage;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sourceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *importedDateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *deleteImage;
 @end
 
-@implementation DetailViewController
+CGFloat const kButtonSize = 40.0f;
 
-- (void)setupShareButton {
-    //self.shareImage = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.shareImage setImage:[GrumpyGifStyleKit imageOfShareWithFrame:CGRectMake(0,
-                                                                                  0,
-                                                                                  80,
-                                                                                  80)]
-                     forState:UIControlStateNormal];
-}
-- (IBAction)shareButton:(UIButton *)sender {
-}
+@implementation DetailViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadDetailView];
-    self.view.backgroundColor = [UIColor randomColor];
-    [self setupShareButton];
+    [self setupButtons];
 }
 -(void)loadDetailView{
     DetailViewInteractor *detailViewInteractor = [[DetailViewInteractor alloc] init];
     ImageEntity *imageDetail = [detailViewInteractor getDataForDetailViewWithImageId:self.imageId];
-
+    
     [self.detailImageView sd_setImageWithURL:[NSURL URLWithString:imageDetail.imageUrl]
-                             completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
-                             }];
+                                   completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                                   }];
+    self.ratingLabel.text = imageDetail.imageRating;
+    self.captionLabel.text = imageDetail.imageCaption;
+    self.sourceLabel.text = imageDetail.imageSource;
+    self.importedDateLabel.text = [NSString stringWithFormat:@"%@", imageDetail.imageImportDate];
 }
+- (void)setupButtons {
+    [self.shareImage setImage:[GrumpyGifStyleKit imageOfShareWithFrame:CGRectMake(0, 0, kButtonSize, kButtonSize)]
+                     forState:UIControlStateNormal];
+    [self.deleteImage setImage:[GrumpyGifStyleKit imageOfEliminateWithFrame:CGRectMake(0, 0, kButtonSize, kButtonSize)]
+                      forState:UIControlStateNormal];}
+- (IBAction)shareGif:(id)sender {
+}
+- (IBAction)deleteImageInCoreData:(id)sender {
+}
+
+
+
 @end
