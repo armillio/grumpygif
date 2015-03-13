91b18fe4d139c9d8f80e0301026dc62e8f97ba4d
Armando <armando@Armando.local>
2015-03-13 21:02:33 +0100

    New cell for when the user opens the app for the first time

---
+++ GrumpyGif/Sections/Cell/Empty/EmptyCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EmptyCollectionViewCell.h
+//  GrumpyGif
+//
+//  Created by Armando on 13/03/15.
+//  Copyright (c) 2015 Armando Carmona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmptyCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ GrumpyGif/Sections/Cell/Empty/EmptyCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  EmptyCollectionViewCell.m
+//  GrumpyGif
+//
+//  Created by Armando on 13/03/15.
+//  Copyright (c) 2015 Armando Carmona. All rights reserved.
+//
+
+#import "EmptyCollectionViewCell.h"
+
+@implementation EmptyCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
