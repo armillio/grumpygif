e2edd461a10ae91f85c1657015da5d8235dee871
Armando <armando@Armando.local>
2015-03-13 01:18:20 +0100

    First part done of the image detail

--- GrumpyGif/Sections/Detail/DetailViewController.m
+++ GrumpyGif/Sections/Detail/DetailViewController.m
@@ -15,22 +15,33 @@
 #import "GrumpyGifStyleKit.h"
 
 @interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *detailImageView;
 @property (weak, nonatomic) IBOutlet UIButton *shareImage;
 @end
 
 @implementation DetailViewController
 
+- (void)setupShareButton {
+    //self.shareImage = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.shareImage setImage:[GrumpyGifStyleKit imageOfShareWithFrame:CGRectMake(0,
+                                                                                  0,
+                                                                                  80,
+                                                                                  80)]
+                     forState:UIControlStateNormal];
+}
+- (IBAction)shareButton:(UIButton *)sender {
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadDetailView];
     self.view.backgroundColor = [UIColor randomColor];
-    self.shareImage.imageView.image = [GrumpyGifStyleKit imageOfShareWithFrame:CGRectMake(0, 0, 30, 30)];
+    [self setupShareButton];
 }
 -(void)loadDetailView{
     DetailViewInteractor *detailViewInteractor = [[DetailViewInteractor alloc] init];
     ImageEntity *imageDetail = [detailViewInteractor getDataForDetailViewWithImageId:self.imageId];
 
-    [self.shareImage.imageView sd_setImageWithURL:[NSURL URLWithString:imageDetail.imageUrl]
+    [self.detailImageView sd_setImageWithURL:[NSURL URLWithString:imageDetail.imageUrl]
                              completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
                              }];
 }
